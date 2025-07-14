bf4181df5311121e442c2798e7968a8c9247464e
Pool <poolespinoza57@gmail.com>
2025-07-13 21:46:09 -0500

    Implementación de NGX Markdown

--- src/app/components/resumen/resumen.ts
+++ src/app/components/resumen/resumen.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { MarkdownComponent } from 'ngx-markdown';
 
 @Component({
   selector: 'app-resumen',
-  imports: [CommonModule],
+  imports: [CommonModule, MarkdownComponent],
   templateUrl: './resumen.html'
 })
 export class ResumenComponent implements OnInit, OnDestroy {
